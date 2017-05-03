70cd5bc58d3ebba0adae5561f5b3f1e65490b35c
lisahoong <lhoong94@gmail.com>
2017-05-03 09:44:01 -0700

    [added] templates for 3 widgets

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ app.get('/*', function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('Express started. Listening on port %s', port);
 
